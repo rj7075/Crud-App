790514835234e41423d46cf06d603735ccca4305
rj7075 <rj7075yadav.com>
2025-06-08 23:33:04 +0530

    third commit

--- crud-node/src/app.js
+++ crud-node/src/app.js
@@ -1,19 +1,17 @@
-const express= require("express");
-const {dbcon}=require("./dbconfig/database.js");
-const routes=require("./routes/routes.js");
+const express = require("express");
+const { dbcon } = require("./dbconfig/database.js");
+const routes = require("./routes/routes.js");
 const bodyParser = require("body-parser");
-const cors=require("cors");
-const app=express();
-const PORT=process.env.PORT || 5000;
+const cors = require("cors");
+const app = express();
+const dotenv=require('dotenv');
+const PORT = process.env.PORT || 5000;
 
 dbcon();
 app.use(bodyParser.json());
 app.use(cors());
-app.use("/api/user" , routes);
+app.use("/api/user", routes);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is Running on PORT : ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is Running on PORT : ${PORT}`);
 });
-
-
-
--- crud-node/src/dbconfig/database.js
+++ crud-node/src/dbconfig/database.js
@@ -1,12 +1,14 @@
-const mongoose=require("mongoose");
-const dbcon= async ()=>{
-    try{
-      await mongoose.connect("mongodb://localhost:27017/crud-operation");
-      console.log("Successfully Connected to MongoDB");
-    }
-    catch(error){
-        console.log(`Mongodb connection error: ${error}`);
-    }
-}
+const mongoose = require("mongoose");
+const dbcon = async () => {
+  try {
+    // await mongoose.connect("mongodb://localhost:27017/crud-operation");
+    await mongoose.connect(
+      "mongodb+srv://rj7075yadav:MGlrLghJubnRTv4S@cluster0.te58gpq.mongodb.net/crud-app"
+    );
+    console.log("Successfully Connected to MongoDB");
+  } catch (error) {
+    console.log(`Mongodb connection error: ${error}`);
+  }
+};
 
-module.exports={mongoose,dbcon}; 
+module.exports = { mongoose, dbcon };
